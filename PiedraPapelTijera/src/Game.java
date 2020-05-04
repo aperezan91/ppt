da9fb8f973f27b6bca350c767148f549eeed03de
apere <apere@DESKTOP-NRC1U9T>
2020-05-04 22:22:07 +0200

    lagartija spock

--- PiedraPapelTijera/src/Game.java
+++ PiedraPapelTijera/src/Game.java
@@ -12,7 +12,7 @@ public class Game {
 	}
 
 	private void welcome() {
-		System.out.println("Bienvenido a nuestro pequeño juego: Piedra Papel Tijeras :-D");
+		System.out.println("Bienvenido a nuestro pequeño juego: Piedra Papel Tijeras, lagartija, spock :-D");
 	}
 
 	private void printHelp() {
